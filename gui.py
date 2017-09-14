ee5be1a8777f476153fb9408679b2f69e7a6868f
ddorn <diego.dorn@free.fr>
2017-09-15 01:52:49 +1000

    more colors and font size change

--- gui.py
+++ gui.py
@@ -3,20 +3,47 @@ from functools import lru_cache
 
 import pygame
 
-from dots.interpreter import AsciiDotsInterpreter
+from dots.vector import Pos
+
+# fixing f****** dpi awareness of my computer
+try:
+    import ctypes
+    ctypes.windll.shcore.SetProcessDpiAwareness(2)
+except:
+    pass
 
 pygame.init()
-pygame.key.set_repeat(100, 50)
-WHITE = (255, 255, 255)
+pygame.key.set_repeat(200, 10)
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+
+FONTNAME = 'assets/monaco.ttf'
 
 REGULAR = 0
 DOT = 1
 BACKGROUND = 2
+OPERATOR = 3
+BRACKETS = 4
+CONTROL_FLOW = 5
+CONTROL_DIR = 6
+DIGIT = 7
+WRAP = 8
+LIBVRAP = 9
+ESCAPE_SEQUANCES = 10
+MODES = 11
 
 COLORS = {
     REGULAR: (248, 248, 242),
-    DOT: (249, 38, 114),
-    BACKGROUND: (39, 40, 34)
+    DOT: (104, 113, 94),
+    BACKGROUND: (39, 40, 34),
+    OPERATOR: (253, 151, 31),
+    BRACKETS: (104, 113, 94),
+    CONTROL_FLOW: (248, 37, 92),
+    CONTROL_DIR: (102, 217, 239),
+    DIGIT: (174, 129, 255),
+    WRAP: (230, 219, 93),
+    LIBVRAP: (166, 226, 46),
+    ESCAPE_SEQUANCES: (249, 38, 114),
+    MODES: (166, 226, 46)
 }
 
 class PygameDebugger:
@@ -29,15 +56,18 @@ class PygameDebugger:
         :param dots.environemt.Env env:
         """
 
+
         self.env = env
 
-        os.environ['SDL_VIDEO_CENTERED'] = '1'
-        self.screen = pygame.display.set_mode((800, 500), pygame.NOFRAME)
-        self.font = pygame.font.Font('assets/monaco.ttf', 16)
+        self.current_tick = -1
+        self.font_size = 16
+        self.char_size = 0, 0   # set by self.set_font
+
+        self.screen = pygame.display.set_mode((0, 0), pygame.NOFRAME)  # type: pygame.SurfaceType
+        self.font = self.new_font(self.font_size)  # type: pygame.font.FontType
         self.clock = pygame.time.Clock()
 
         self.ticks = []
-        self.current_tick = -1
 
     def run(self):
         """Start the debugger. stop it with io.on_finish()"""
@@ -55,9 +85,14 @@ class PygameDebugger:
                     self.env.io.on_finish()
                     return
                 elif e.key == pygame.K_RIGHT:
-                    self.current_tick += 1
+                    # move 5 steps if ctrl pressed
+                    self.current_tick += 1 + 4*(e.mod & pygame.KMOD_CTRL != 0)
                 elif e.key == pygame.K_LEFT:
-                    self.current_tick = max(0, self.current_tick - 1)
+                    self.current_tick = max(0, self.current_tick - 1 - 4*(e.mod & pygame.KMOD_CTRL != 0))
+                elif e.key == pygame.K_EQUALS:  # I would like the + but apparently it doesn't work
+                    self.font = self.new_font(self.font_size + 1)
+                elif e.key == pygame.K_MINUS:
+                    self.font = self.new_font(self.font_size - 1)
 
         while self.current_tick >= len(self.ticks) and not self.env.io.finished:
             tick = self._get_new_tick()
@@ -71,22 +106,54 @@ class PygameDebugger:
         else:
             dot_pos = {dot.pos for dot in self.ticks[self.current_tick]}
 
+        xstep = Pos(1, 0)
+
         for row, line in enumerate(self.env.world.map):
             for col, char in enumerate(line):
-                c = str(char)
+                c = char
+                pos = Pos(col, row)
 
-                if (col, row) in dot_pos:
-                    surf = self._get_surface_for_char(c, DOT)
+                if char.isOper():
+                    color = OPERATOR
+                elif c in '[{' and self.env.world.does_loc_exist(pos + xstep) and self.env.world.get_char_at(pos + xstep).isOper():
+                    color = BRACKETS
+                elif c in '}]' and self.env.world.does_loc_exist(pos - xstep) and self.env.world.get_char_at(pos - xstep).isOper():
+                    color = BRACKETS
+                elif c in '~*':
+                    color = CONTROL_FLOW
+                elif c in '<>v^':
+                    color = CONTROL_DIR
+                elif c.isdigit():
+                    color = DIGIT
+                elif char.isWarp():
+                    color = WRAP
+                elif char.isLibWarp():
+                    color = LIBVRAP
+                elif char in '@#$':
+                    color = MODES
                 else:
-                    surf = self._get_surface_for_char(c, REGULAR)
+                    color = REGULAR
 
-                pos = 5 + 11 * col, 5 + 22 * row
+                if pos in dot_pos:
+                    surf = self._get_surface_for_char(c, color, DOT)
+                else:
+                    surf = self._get_surface_for_char(c, color, BACKGROUND)
+
+                pos = 5 + self.char_size[0] * col, 5 + self.char_size[1] * row
                 self.screen.blit(surf, pos)
+
+    def new_font(self, size):
+        size = min(60, max(2, size))  # clamp
+        self.font_size = size
+        self._get_surface_for_char.cache_clear()
+        font = pygame.font.Font(FONTNAME, size)
+        self.char_size = font.size(".")
+        return font
 
     def _get_new_tick(self):
         """Get a new tick from the interpreter, it can be none if there is no new tick yet"""
         return self.env.io.get_tick(wait=True)
 
     @lru_cache(maxsize=None)
-    def _get_surface_for_char(self, char, color):
-        return self.font.render(char, True, COLORS[color], COLORS[BACKGROUND])
+    def _get_surface_for_char(self, char, color, background):
+        return self.font.render(char, True, COLORS[color], COLORS[background])
